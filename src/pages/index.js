9216ad4eba70d57f6fcc48f0e82cccdabd81d23b
Nick Tsai <nicktsai1026@gmail.com>
2022-10-25 23:34:45 -0400

    reorder sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
   ContactSection,
   HeroSection,
   InterestsSection,
-  Page,
   ProjectsSection,
+  Page,
   Seo,
 } from "gatsby-theme-portfolio-minimal";
+import ExperienceSection from '../components/experience';
 
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Nick Tsai Portfolio" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <AboutSection sectionId="about" heading="About Me" />
+        <ExperienceSection sectionId="experience" heading="Experience"/>
+        <InterestsSection sectionId="skills" heading="Skills" />
+        <ProjectsSection sectionId="projects" heading="My Projects" />
+        <ContactSection sectionId="contact" heading="Contact Me" />
       </Page>
     </>
   );
