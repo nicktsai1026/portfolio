17ab95e7dd069d8448266b42eecc69bd5c1c14a2
Nick Tsai <nicktsai1026@gmail.com>
2022-10-25 23:35:54 -0400

    add new dependencies and deploy script

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,13 @@
 module.exports = {
+  pathPrefix: "/portfolio",
   plugins: [
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
+          favicon: "./content/images/logo.png", // Path is relative to the root
+          siteName: "Nick's Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
@@ -14,10 +15,10 @@ module.exports = {
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
-        blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
+        // blogSettings: {
+        //   path: "/blog", // Defines the slug for the blog listing page
+        //   usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        // },
         // googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
         //     anonymize: true, // Default true
