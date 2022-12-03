3b1aa193e07671393dbd29e1bf5bec8d9dc9640f
Nick Tsai <nicktsai1026@gmail.com>
2022-12-03 00:18:16 -0500

    remove summary query

--- src/sections/data.js
+++ src/sections/data.js
@@ -9,7 +9,6 @@ export const ExperienceDataSource = () => {
                         duties
                         name
                         period
-                        summary
                         title
                         website
                     }
