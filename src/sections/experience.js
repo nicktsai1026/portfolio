ef5ad8ef655f48c89a14251e61287c32a0470f00
Nick Tsai <nicktsai1026@gmail.com>
2022-12-03 00:19:09 -0500

    render experience timeline

--- src/sections/experience.js
+++ src/sections/experience.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Animation, Section } from "gatsby-theme-portfolio-minimal";
 import { ExperienceDataSource } from "./data";
-// import { Timeline } from "../components/Timeline";
+import { Timeline } from "../components/Timeline";
 
 export default function AboutSection(props) {
     const companies = ExperienceDataSource().experienceJson.companies;
@@ -9,18 +9,8 @@ export default function AboutSection(props) {
     return (
         <Animation type="fadeUp">
             <Section anchor={props.sectionId} heading={props.heading}>
-                {companies.map(company => (
-                    <div>
-                        {/* <Timeline period={company.period} company={company.name} title={company.title} website={company.website} /> */}
-                        <h2>Company: {company.name}</h2>
-                        <h4>Title: {company.title}</h4>
-                        <label>{company.period}</label>
-                        <ul>
-                            {company.duties.map(duty => (
-                                <li>{duty}</li>
-                            ))}
-                        </ul>
-                    </div>
+                {companies.map((company, index) => (
+                    <Timeline company={company} key={index}/>
                 ))}
             </Section>
         </Animation>
